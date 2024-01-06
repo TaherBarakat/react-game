5549ebd19650b294e660fa11238e8e6a4dc3e049
Taher Barakat <taher.barakat12223@gmail.com>
2024-01-06 18:12:01 +0300

    handle the input where I set the player name using useState.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+     const [playerName, setPlayerName] = useState(null);
+     const [sub, setSub] = useState(false);
+
+     function handleSub() {
+          setSub(true);
+     }
+     return (
+          <section id="player">
+               <h2>Welcome {sub ? playerName : "unknown entity"} </h2>
+               <p>
+                    <input
+                         type="text"
+                         value={playerName}
+                         onChange={(e) => {
+                              setPlayerName(e.target.value);
+                              setSub(false);
+                         }}
+                    />
+                    <button onClick={handleSub}>Set Name</button>
+               </p>
+          </section>
+     );
 }
